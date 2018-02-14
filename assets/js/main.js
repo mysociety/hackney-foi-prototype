1c70b9220fd065abb988867f754ebd776ac4ad5c
Zarino Zappia <mail@zarino.co.uk>
2018-02-14 16:17:45 +0000

    Templates for FOI process

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,10 @@
 $(function(){
-  // Example script
+    $('[data-aside]').each(function(){
+        var $control = $(this);
+        var $aside = $( $(this).attr('data-aside') );
+
+        $control.on('focus', function(){
+            $aside.addClass('form-aside--active');
+        });
+    });
 });
